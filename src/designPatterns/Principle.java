751886d9bbe1d9b42e5a45f2453d535bb7232cef
xiongyi <xiongyi940803@qq.com>
2017-08-30 16:12:35 +0800

    设计模式六大原则

---
+++ src/designPatterns/Principle.java
@@ -0,0 +1,14 @@
+package designPatterns;
+
+public class Principle {
+	/*
+	 * 设计模式的六大原则
+	 * 1、开闭原则（open close principle）
+	 * 		对扩展开放，对修改关闭
+	 * 2、里氏代换原则
+	 * 3、依赖倒转原则
+	 * 4、接口隔离原则
+	 * 5、迪米特法则（最少知道原则）
+	 * 6、合成服用原则
+	 * */
+}
