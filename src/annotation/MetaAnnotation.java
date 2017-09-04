d3e66f6406c6d477f04b7906d99c7456ebfc4913
xiongyi <xiongyi940803@qq.com>
2017-09-04 12:01:14 +0800

    学习java注解

---
+++ src/annotation/MetaAnnotation.java
@@ -0,0 +1,16 @@
+package annotation;
+
+public class MetaAnnotation {
+	/*
+	 * 元注解
+	 * Java 5.0定义了4个标准的meta-annotation类型
+	 * 被用来提供元注解和相关定义注解的语法
+	 * @Target
+	 * @Retention
+	 * @Documented
+	 * @Inherited
+	 * */
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/annotation/Target.java
@@ -0,0 +1,27 @@
+package annotation;
+
+public class Target {
+	
+	/*
+	 * @Target说明了Annotation所修饰的对象
+	 * 可被用于package、type（类、接口、枚举、Annotation类型）、类型成员（方法、构造方法、成员变量、枚举值）、方法参数和本地变量
+	 * 在Annotation类型的声明中使用了target可更加明晰其修饰的目标
+	 * 
+	 * 作用：用于描述注解的使用范围（即：被描述的注解可以用在什么地方）
+	 * 
+	 * 取值（Element）：
+	 * 
+	 * 　 1.CONSTRUCTOR:用于描述构造器
+　　　*　  2.FIELD:用于描述域
+　　　*　  3.LOCAL_VARIABLE:用于描述局部变量
+　　　*　  4.METHOD:用于描述方法
+　　　*　  5.PACKAGE:用于描述包
+　　　*　  6.PARAMETER:用于描述参数
+　　　*　  7.TYPE:用于描述类、接口(包括注解类型) 或enum声明
+	 * 
+	 * */
+	
+	public static void main(String[] args) {
+		
+	}
+}
