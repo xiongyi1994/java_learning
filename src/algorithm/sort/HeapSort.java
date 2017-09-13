c60b714bb197220033d3bf6288c7adf090f451a7
xiongyi <xiongyi940803@qq.com>
2017-09-13 16:48:39 +0800

    堆排序

---
+++ src/algorithm/sort/HeapSort.java
@@ -0,0 +1,43 @@
+package algorithm.sort;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] a = {5, 2, 7, 3, 0, 0, 0, 0, 0,1,2,3,4,5,67,8,456,45,645,645,645,6,456,};
+        heapSort(a);
+        print(a);
+    }
+
+    private static void heapSort(int[] a) {
+        for (int i = a.length - 1; i >= 0; --i) {
+            heapAdjust(a, i);
+            swap(a, 0, i);
+        }
+    }
+
+    private static void heapAdjust(int[] a, int m) {
+        for (int i = m / 2 - 1; i >= 0; --i) {
+            int max = i;
+            if (a[i] < a[2 * i]) {
+                max = 2 * i;
+            }
+            if (2 * i + 1 <= m) {
+                if (a[max] < a[2 * i + 1]) {
+                    max = 2 * i + 1;
+                }
+            }
+            swap(a, i, max);
+        }
+    }
+
+    private static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    private static void print(int[] a) {
+        for (int anA : a) {
+            System.out.print(anA + "、");
+        }
+    }
+}
