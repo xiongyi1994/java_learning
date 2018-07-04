9c7ff00f3789416965418e0d169a8aacf69b59e8
xiongyi <xiongyi940803@qq.com>
2018-07-04 12:11:50 +0800

    设计模式-饿汉式单例模式

---
+++ src/designPatterns/Singleton05.java
@@ -0,0 +1,17 @@
+package designPatterns;
+
+public class Singleton05 {
+	/*
+	 * 饿汉式单例
+	 * 在类初始化时，已经自行实例化
+	 * 饿汉式天生就是线程安全的
+	 * */
+	
+	private Singleton05 () {};
+	private static final Singleton05 singleton = new Singleton05();
+	
+	public static Singleton05 getInstance() {
+		return singleton;
+	}
+	
+}
