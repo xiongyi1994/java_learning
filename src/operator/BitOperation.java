95bcd0da8ac76864cf80c84461779b72b32f87dc
xiongyi <xiongyi940803@qq.com>
2018-07-17 20:01:55 +0800

    格式优化

--- src/operator/BitOperation.java
+++ src/operator/BitOperation.java
@@ -2,6 +2,7 @@ package operator;
 
 public class BitOperation {
 	public static void main(String[] args) {
+
 		/*
 		 * & 按位与运算符
 		 * */
@@ -10,14 +11,12 @@ public class BitOperation {
 		int b = 11;
 		System.out.println(Integer.toString(a, 2)); // => 1010
 		System.out.println(Integer.toString(b, 2)); // => 1011
-		System.out.println(a & b);
-		
+		System.out.println(a & b); // => 10
 		
 		/*
 		 * >> 右移 
 		 * 用符号位填充高位
 		 * */
-		
 		System.out.println(a >> 1); // -> 5
 		System.out.println(a >> 2); // -> 2
 		
