9b224bcac847c3564f985d0575314c23d6d097b0
xiongyi <xiongyi940803@qq.com>
2018-07-19 09:53:06 +0800

    元注解

--- src/annotation/MetaAnnotation.java
+++ src/annotation/MetaAnnotation.java
@@ -5,10 +5,10 @@ public class MetaAnnotation {
 	 * 元注解
 	 * Java 5.0定义了4个标准的meta-annotation类型
 	 * 被用来提供元注解和相关定义注解的语法
-	 * @TargetAnnotation
-	 * @RetentionAnnotation
+	 * @Target
+	 * @Retention
 	 * @Documented
-	 * @InheritedAnnotation
+	 * @Inherited
 	 * */
 	public static void main(String[] args) {
 		
