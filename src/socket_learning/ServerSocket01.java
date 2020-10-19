1bca840bea50445f7bc7ff0d9b1314c3f5f1027f
DESKTOP-OMQF16M\xiongyi <xiongyi940803@qq.com>
2020-10-19 13:40:26 +0800

    代码格式化

--- src/socket_learning/ServerSocket01.java
+++ src/socket_learning/ServerSocket01.java
@@ -6,20 +6,20 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerSocket01 {
-	public static void main(String[] args) {
-		try{
-		  // 初始化服务端并且监听8080端口
-			ServerSocket server = new ServerSocket(8080);
-			// 等待客户连接
-			Socket socket = server.accept();
-			// 获取输入流，并指定统一的编码格式
-			BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-			// 读取一行数据
-			String line;
-			// 通过while循环不断读取信息
-      while ((line = is.readLine()) != null) {
-        System.out.println(line);
-      }
+    public static void main(String[] args) {
+        try {
+            // 初始化服务端并且监听8080端口
+            ServerSocket server = new ServerSocket(8080);
+            // 等待客户连接
+            Socket socket = server.accept();
+            // 获取输入流，并指定统一的编码格式
+            BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+            // 读取一行数据
+            String line;
+            // 通过while循环不断读取信息
+            while ((line = is.readLine()) != null) {
+                System.out.println(line);
+            }
 			/*PrintWriter pw = new PrintWriter(socket.getOutputStream());
 			pw.println("received data: " + line);
 			pw.flush();
@@ -27,8 +27,8 @@ public class ServerSocket01 {
 			is.close();
 			socket.close();
 			server.close();*/
-		}catch(Exception e){
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
